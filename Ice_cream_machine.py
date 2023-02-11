a29421bd89b32b7b5aaedd71abd9078c1c21d166
Hazem-Az-Eldin <hazemahmedfreestyle@gmail.com>
2023-02-11 14:37:29 +0000

    Ice Cream machine

---
+++ Ice_cream_machine.py
@@ -0,0 +1,18 @@
+class IceCreamMachine:
+    
+    def __init__(self, ingredients, toppings):
+        self.ingredients = ingredients
+        self.toppings = toppings
+        
+    def scoops(self):
+        scoops = []
+        for y in self.toppings:
+            for x in self.ingredients:
+                scoops.append([x,y])
+        
+        return scoops
+
+
+if __name__ == "__main__":
+    machine = IceCreamMachine(["vanilla", "chocolate"], ["chocolate sauce"])
+    print(machine.scoops()) #should print: [['vanilla', 'chocolate sauce'], ['chocolate', 'chocolate sauce']]
